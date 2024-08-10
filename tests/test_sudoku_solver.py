9cbf31ef2de53fb9d84eda7951e59f9f63aabae8
yyeeeeeeeeee <168656207+yyeeeeeeeeee@users.noreply.github.com>
2024-08-10 17:12:47 +0800

    Create test_sudoku_solver.py

---
+++ tests/test_sudoku_solver.py
@@ -0,0 +1,61 @@
+import pytest
+from Solver import Solver
+
+def test_solve_valid_sudoku():
+
+    info = {
+    'game': 'sudoku', # sudoku, stars, skyscrapers
+    'GRID_LEN': 9,
+    'SQUARE_LEN': 3,
+    'NUM_STARS': 1
+    }
+
+    if len(sys.argv) > 1:#TODO check + controllo games
+        if sys.argv[1] is not None:
+            info['game'] = sys.argv[1]
+        if sys.argv[2] is not None:
+            info['GRID_LEN'] = int(sys.argv[2])
+        if len(sys.argv) > 3 and sys.argv[3] is not None:
+            info['SQUARE_LEN'] = int(sys.argv[3])
+
+    solver = Solver(info)
+
+    puzzle = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+    expected_solution = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+    solution = solver.solveGame(puzzle)
+    assert solution == expected_solution
+
+def test_invalid_sudoku():
+    puzzle = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 7]  # Invalid row (duplicate 7)
+    ]
+    with pytest.raises(ValueError):
+        solver.solveGame(puzzle)
